db6249a94ef7a3f49e59722604a13aa47a8d61f1
Phanthep Supaveerasathien <159607972+PhanthepIT@users.noreply.github.com>
2025-03-21 08:40:00 +0700

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -82,7 +82,7 @@ const Header = () => {
                                     <a href="https://shop.line.me/@mat.th" target="_blank"><FaLine /></a>
                                 </div>
         <div className='flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute before:absolute before:h-[18px] before:bg-[#afafaf] before:w-[1px] before:-left-[20px]'>
-            <img src="http://localhost:3000/images/language.png" alt="" />
+            <img src="https://micro-agrotec.com/images/language.png" alt="" />
             <span><IoMdArrowDropdown /></span>
             <ul className='absolute invisible transition-all top-12 rounded-sm duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10'>
             <li>Thai</li>
@@ -116,7 +116,7 @@ const Header = () => {
             <div className='md-lg:w-full w-3/12 md-lg:pt-4'>
             <div className='flex justify-between items-center'>
                 <Link to='/'>
-                    <img src="http://localhost:3000/images/logo.png" alt="" />
+                    <img src="https://micro-agrotec.com/images/logo.png" alt="" />
                 </Link>
                 <div className='justify-center items-center w-full h-[40px] bg-white text-slate-600 border border-slate-600 rounded-sm cursor-pointer lg:hidden md-lg:flex xl:hidden hidden' onClick={() => setShowShidebar(false)}>
                     <span> <FaList/> </span>
@@ -195,7 +195,7 @@ card_product_count !== 0 && <div className='w-[20px] h-[20px] absolute bg-red-50
         <div className={`w-[300px] z-[9999] transition-all duration-200 fixed ${showShidebar ? '-left-[300px]' : 'left-0 top-0'} overflow-y-auto bg-white h-screen py-6 px-8 `}>
                 <div className='flex justify-start flex-col gap-6'>
                 <Link to='/'>
-                    <img src="http://localhost:3000/images/logo.png" alt="" />
+                    <img src="https://micro-agrotec.com/images/logo.png" alt="" />
                 </Link>
     <div className='flex justify-start items-center gap-10'>
     <div className='flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute '>
